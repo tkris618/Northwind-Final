057a29390074c9c307b6fd054aeb2de1bb02bf45
jgrissom <jgrissom91@gmail.com>
2023-04-06 13:27:46 -0500

    [5.05] Add Customer (Company Name only)
    - Data Context: AddCustomer method
    - Customer/Register method that responds to HTTP POST requests

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,5 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 
 public class CustomerController : Controller
 {
-    public IActionResult Register() => View();
+    // this controller depends on the DataContext
+        private DataContext _dataContext;
+        public CustomerController(DataContext db) => _dataContext = db;
+        public IActionResult Register() => View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.AddCustomer(customer);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
 }
