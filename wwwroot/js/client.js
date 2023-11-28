ca8bf6d53aaf404ad81455cee6fdcd862479cc34
Samuel Hooper <shooper3@my.wctc.edu>
2023-11-28 10:43:08 -0600

    Added add discount button

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function () {
     $(document).keydown(function(e) {
         // ESCAPE key pressed
         if (e.keyCode == 27) {
-            $('#liveToast').toast({ autohide: false }).toast('hide');
+            $('#home_toast').toast({ autohide: false }).toast('hide');
         }
     });
 
@@ -10,13 +10,13 @@ $(function(){
         e.preventDefault();
         
         // set name
-        $('#product-name').text("" + $(this).data("product"));
+        $('#home_toast_header').text("" + $(this).data("product"));
 
         // set code
-        $('#code').text("" + $(this).data("code"));
+        $('#home_toast_body').text("" + $(this).data("code"));
 
         // display the toast to user
-        $('#liveToast').toast({ autohide: false }).toast('show');
+        $('#home_toast').toast({ autohide: false }).toast('show');
 
     });
 });
