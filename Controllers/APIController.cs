df540dedfe2d94dc797cf5bc08320ab7ca65ff0e
Samuel Hooper <shooper3@my.wctc.edu>
2023-11-28 08:16:16 -0600

    Updated with Class Code

---
+++ Controllers/APIController.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class APIController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private DataContext _dataContext;
+        public APIController(DataContext db) => _dataContext = db;
+
+        [HttpGet, Route("api/product")]
+        // returns all products
+        public IEnumerable<Product> Get() => _dataContext.Products.OrderBy(p => p.ProductName);
+        [HttpGet, Route("api/product/{id}")]
+        // returns specific product
+        public Product Get(int id) => _dataContext.Products.FirstOrDefault(p => p.ProductId == id);
+        [HttpGet, Route("api/product/discontinued/{discontinued}")]
+        // returns all products where discontinued = true/false
+        public IEnumerable<Product> GetDiscontinued(bool discontinued) => _dataContext.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+        [HttpGet, Route("api/category")]
+        // returns all categories
+        public IEnumerable<Category> GetCategory() => _dataContext.Categories.Include("Products").OrderBy(c => c.CategoryName);
+        [HttpGet, Route("api/category/{CategoryId}/product")]
+        // returns all products in a specific category
+        public IEnumerable<Product> GetByCategory(int CategoryId) => _dataContext.Products.Where(p => p.CategoryId == CategoryId).OrderBy(p => p.ProductName);
+        [HttpGet, Route("api/category/{CategoryId}/product/discontinued/{discontinued}")]
+        // returns all products in a specific category where discontinued = true/false
+        public IEnumerable<Product> GetByCategoryDiscontinued(int CategoryId, bool discontinued) => _dataContext.Products.Where(p => p.CategoryId == CategoryId && p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+        [HttpPost, Route("api/addtocart")]
+        // adds a row to the cartitem table
+        public CartItem Post([FromBody] CartItemJSON cartItem) => _dataContext.AddToCart(cartItem);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
 
 // Connection info stored in appsettings.json
 IConfiguration configuration = new ConfigurationBuilder()
@@ -6,14 +8,20 @@ IConfiguration configuration = new ConfigurationBuilder()
     .Build();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+// Ignore circular reference cycles
+builder.Services.AddControllersWithViews().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Register the DataContext service
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(configuration["Data:Northwind:ConnectionString"]));
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(configuration["Data:AppIdentity:ConnectionString"]));
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
